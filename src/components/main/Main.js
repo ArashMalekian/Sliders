fe876557a3d95ae1fb10c30b78a09386fb4ca840
ArashMalekian <malekianarash.am@gmail.com>
2022-06-10 17:18:03 +0430

    create content for sliderone

---
+++ src/components/main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SliderOne } from './sliderone/SliderOne'
+
+export const Main = () => {
+    return (
+        <>
+           <SliderOne />
+        </>
+    )
+}
