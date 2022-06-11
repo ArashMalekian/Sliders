6551a0422c2d1ff4f61e7ea1e0fa85123dcd9a90
ArashMalekian <malekianarash.am@gmail.com>
2022-06-11 19:47:44 +0430

    complete sliderone

--- src/components/main/sliderone/SliderOne.js
+++ src/components/main/sliderone/SliderOne.js
@@ -1,7 +1,27 @@
 import React , {useEffect} from 'react'
 import {useDispatch , useSelector } from 'react-redux'
 import { getFilms } from '../../../redux/getfilms/GetFilmsAction'
+import Carousel from 'react-elastic-carousel'
+import  { consts } from 'react-elastic-carousel';
+import classes from './SliderOne.module.scss'
 
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 1, pagination: false },
+    { width: 850, itemsToShow: 3 },
+    { width: 1150, itemsToShow: 4, itemsToScroll: 1 },
+    { width: 1450, itemsToShow: 4 },
+    { width: 1750, itemsToShow: 5 },
+  ]
+
+const myArrow = ({ type, onClick, isEdge }) => {
+    const pointer = type === consts.PREV ? 'قبل' : 'بعد'
+    return (
+      <button className={classes.arrbtn} onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </button>
+    )
+}
 
 export const SliderOne = () => {
     const dispatch = useDispatch()
@@ -18,11 +38,14 @@ export const SliderOne = () => {
             {
                 state.loading ?
                 <h1>please wait . . . </h1> :
+                <Carousel className={classes.carouselcontainer}  pagination={false}  breakPoints={breakPoints} focusOnSelect={true} renderArrow={myArrow} >{
                 state.data.map(item => 
-                    <h1>
-                        {item.id}
-                    </h1>
-                )
+                        <div className={classes.carouselitem} key={item.id} >
+                            <img alt={item.id} src={item.poster} />
+                            {item.title}
+                        </div>
+                )}
+                </Carousel>
             }
         </div>
     )
